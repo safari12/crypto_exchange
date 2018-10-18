bd9d0948643cb8c8ccb7e9ddeb60eff47937cf95
safari <rsafari.s@gmail.com>
2018-10-18 03:33:33 -0500

    fixed syntax

--- lib/crypto_exchange/api/config.ex
+++ lib/crypto_exchange/api/config.ex
@@ -1,17 +1,17 @@
 defmodule CryptoExchange.API.Config do
   defmacro __using__(opts) do
     quote do
-
-      exchange = unquote(opts)[:exchange]
-
-      def base_url() do
-        Application.fetch_env!(exchange, :api)[:base_url]
+      def base_url do
+        Application.fetch_env!(exchange_config_key(), :api)[:base_url]
       end
 
-      def token() do
-        Application.fetch_env!(exchange, :api)[:token]
+      def token do
+        Application.fetch_env!(exchange_config_key(), :api)[:token]
       end
 
+      defp exchange_config_key do
+        unquote(opts)[:exchange]
+      end
     end
   end
 end
