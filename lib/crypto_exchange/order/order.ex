4c235d73f340c37727217a0a789f24f430cd4365
safari <rsafari.s@gmail.com>
2018-10-24 22:54:30 -0500

    removed unnecessary pair field in order structure

--- lib/crypto_exchange/order/order.ex
+++ lib/crypto_exchange/order/order.ex
@@ -3,16 +3,12 @@ defmodule CryptoExchange.Order do
   Data structure presenting a buy or sell order for a market
   """
 
-  alias CryptoExchange.Pair
-
   @type t :: %__MODULE__{
-    pair: Pair.t,
     quantity: float,
     price: float
   }
 
   defstruct [
-    :pair,
     :quantity,
     :price
   ]
